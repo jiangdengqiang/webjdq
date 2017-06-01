e7130acfbf759ab0d786460c078d509443692732
jiangdengqiang <dengqiang.jiang@atop-online.de>
2017-06-01 11:42:56 +0800

    css

--- html/js/index.js
+++ html/js/index.js
@@ -13,7 +13,26 @@
 			$(".callback").on("click",function(){
 				$("body,html").animate({scrollTop:"0px"},500);
 			})
+		},
+		heart:function(){
+			$("body").on("click",function(event){
+				var heart = "<span class='glyphicon glyphicon-heart heart'></span>",
+					res="",
+					top = $(document).scrollTop(),
+					y_top = (event || window.event).clientY,
+					x = (event || window.event).clientX,
+					y = y_top+top;
+				$("body").append(heart);
+				location(res);
+			})
+			var location  = function(colors){
+				var color_val = ["#99cc33","#ff9933","#ff99cc","#99ccff","#f08080","#8EE5EE","#8470FF","#CD3700","#F5DEB3","#e7afaf"];
+				colors = Math.ceil(Math.random()*10);
+				console.log(colors);
+			}
+
 		}
 	}
 	method.scrollHeight();
+	method.heart();
 })(jQuery);
